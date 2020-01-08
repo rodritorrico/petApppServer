c66780856eb0657d87ec48ba05730f10881517d5
whiletruedothis <rodrigo.torricodelcastillo@gmail.com>
2020-01-08 05:59:34 -0400

    added principal lines from master

--- adapters/Express.js
+++ adapters/Express.js
@@ -1,7 +1,7 @@
+const Mongo = require('./Mongo');
 const express = require('express');
 let app = express();
 
-const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const cors = require('cors');
@@ -9,18 +9,44 @@ app.use(bodyParser.text());
 app.use(cors());
 
 
-
-
 class Express{
 
-    constructor(){
-      
+    constructor(dataBase){
+      this.userWantsToFeedPet = false;
+      this.dataBase = dataBase;
+      this.feedTimes = 0;
+      this.percentage = 10;
     }
 
     async defineRoutes(){
-    
-        app.get("/hola",(request, response)=>{
-            response.send('hola');
+        
+        app.get('/feedPet',(request, response)=>{
+            if(this.userWantsToFeedPet){
+                response.send(true);
+            }else{
+                response.send(false);
+            }
+        })
+
+        app.post('/thingData', (request, response)=>{
+            let thingData = request.body;
+            this.dataBase.registerObject({data: thingData},'ThingData');
+            response.sendStatus(200);
+        })
+
+        app.get('/getFeedTimes', (request, response)=>{
+            response.send(this.feedTimes.toString());
+        })
+
+        app.get('/getPlatePercentage', (request, response) =>{
+            response.send(this.percentage.toString());
+        })
+
+        app.post('/giveFood', (request,response)=>{
+            let giveFoodData = request.body;
+            console.log(giveFoodData);
+            // this.userWantsToFeedPet =true;
+            response.sendStatus(200);
         })
 
 
--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const Express = require('./adapters/Express');
 const ServerRepository = require('./repositories/ServerRepository');
 const Mongo = require('./adapters/Mongo');
 
-var mongo = new Mongo('mongodb+srv://admin:admin@petapp-cbmfv.mongodb.net/test?retryWrites=true&w=majority','petApp');
+var mongo = new Mongo('mongodb+srv://admin:admin@cluster0-bww9t.mongodb.net/test?retryWrites=true&w=majority','petApp');
 var mongoConected = mongo.startConection();
 
 
 
 mongoConected.then(()=>{
-    let express = new Express();
+    let express = new Express(mongo);
     let serverRepository= new ServerRepository(express);
     var serverPort = process.env.PORT || '3000';
     serverRepository.initializeServer(serverPort);
